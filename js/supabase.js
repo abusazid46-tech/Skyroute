800705da23fa0dda959b5feea244161b95f5d5d8
abusazid46-tech <abusazid46@gmail.com>
2026-03-15 23:42:58 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL="YOUR_URL"
-const SUPABASE_KEY="YOUR_PUBLIC_KEY"
+const SUPABASE_URL="https://pierprjgeypcsvdlojlt.supabase.co"
+const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpZXJwcmpnZXlwY3N2ZGxvamx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1ODE1ODMsImV4cCI6MjA4OTE1NzU4M30.KGz_UBwqWkO8qE4g2mQHy7ktD1ZMFTw2a9uJKuzfjCk"
 
 const client=supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
