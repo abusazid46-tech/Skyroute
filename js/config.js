07cc599583e36bbdc7572a5899d01b036cfc4a3c
abusazid46-tech <abusazid46@gmail.com>
2026-03-15 11:48:51 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Supabase configuration
+const SUPABASE_CONFIG = {
+    url: 'https://pierprjgeypcsvdlojlt.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpZXJwcmpnZXlwY3N2ZGxvamx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1ODE1ODMsImV4cCI6MjA4OTE1NzU4M30.KGz_UBwqWkO8qE4g2mQHy7ktD1ZMFTw2a9uJKuzfjCk'
+};
